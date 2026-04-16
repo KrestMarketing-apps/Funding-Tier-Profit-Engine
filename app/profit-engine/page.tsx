2097b6510046b522784ca43bac277253eb4568e8
KrestMarketing-apps <vendors@krestmarketing.com>
2026-04-16 12:55:58 -0700

    Create page.tsx

---
+++ app/profit-engine/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import FundingTierProfitabilityBalancer from "../../components/FundingTierProfitabilityBalancer";
+
+export default function ProfitEnginePage() {
+  return <FundingTierProfitabilityBalancer />;
+}
